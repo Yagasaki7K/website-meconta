598613dd24db63f24ccf007639b6ef09e75be20b
Anderson Marlon <anderson18.marlon@gmail.com>
2024-02-07 18:46:19 -0300

    Added: Mockup

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,12 +3,30 @@ import { useState } from 'react'
 
 const Navigation = () => {
     const [hiddenUserMenu, setHiddenUserMenu] = useState(false)
+    const [isOpenModalAdded, setIsOpenModalAdded] = useState(false)
+    const [isOpenModalRemove, setIsOpenModalRemove] = useState(false)
 
     function handleUserMenu() {
         if (hiddenUserMenu === true) {
             setHiddenUserMenu(false)
         } else {
             setHiddenUserMenu(true)
+        }
+    }
+
+    function handleModalAdded() {
+        if (isOpenModalAdded) {
+            setIsOpenModalAdded(false);
+        } else {
+            setIsOpenModalAdded(true);
+        }
+    }
+
+    function handleModalRemoved() {
+        if (isOpenModalAdded) {
+            setIsOpenModalRemove(false);
+        } else {
+            setIsOpenModalRemove(true);
         }
     }
 
@@ -20,8 +38,8 @@ const Navigation = () => {
                 <a href='/dashboard'><img src="/logo.png" alt="Logotipo" /></a>
                 <a href='/dashboard'><li><i className="uil uil-chart-growth" /> Central de Relatórios</li></a>
                 <a href='/dashboard'><li><i className="uil uil-tag-alt" /> Cadastrar Categorias</li></a>
-                <a href='/dashboard'><li><i className="uil uil-plus-circle" /> Adicionar Entrada</li></a>
-                <a href='/dashboard'><li><i className="uil uil-minus-circle" /> Adicionar Saída</li></a>
+                <a onClick={handleModalAdded}><li ><i className="uil uil-plus-circle" /> Adicionar Entrada</li></a>
+                <a onClick={handleModalRemoved}><li><i className="uil uil-minus-circle" /> Adicionar Saída</li></a>
                 <a href='/dashboard'><li><i className="uil uil-file-search-alt" /> Gerar PDF</li></a>
                 <li><i className="uil uil-signout" /> Logout</li>
             </ul>
@@ -30,13 +48,78 @@ const Navigation = () => {
             <div className="sideMenu">
                 {hiddenUserMenu ? (
                     <>
-                        <a className="addBtn"><i className="uil uil-plus-circle" /></a>
-                        <a className="removeBtn"><i className="uil uil-minus-circle" /></a>
+                        <button className="addBtn" onClick={handleModalAdded}><i className="uil uil-plus-circle" /></button>
+                        <button className="removeBtn" onClick={handleModalRemoved}><i className="uil uil-minus-circle" /></button>
                     </>) : null
                 }
                 <a className="whatsappBtn"><i className="uil uil-whatsapp" /></a>
                 <button className="logoBtn" onClick={handleUserMenu}><i className="uil uil-usd-circle" /></button>
             </div>
+
+            {
+                isOpenModalAdded ? (
+                    <ModalDetails>
+                        <div className="modal-close" onClick={() => setIsOpenModalAdded(false)}><i className="uil uil-times-circle" /></div>
+                        <div className="modal-content">
+                            <h3>Adicionar Entrada</h3>
+                            <i>Entrada são valores que entraram na conta</i>
+
+                            <label htmlFor="">Qual é a categoria?</label>
+                            <select name="" id="">
+                                <option value="">Salário</option>
+                                <option value="">Contas à Receber</option>
+                                <option value="">Dinheiro Emprestado</option>
+                            </select>
+
+                            <label htmlFor="">Informe a data</label>
+                            <input type="date" name="" id="" />
+
+                            <label htmlFor="">Informe o valor</label>
+                            <input type="text" name="" id="" />
+
+                            <div className="buttons">
+                                <button className="send">Enviar</button>
+                                <button className="clear">Limpar</button>
+                            </div>
+                        </div>
+                    </ModalDetails>
+                ) : null
+            }
+
+            {
+                isOpenModalRemove ? (
+                    <ModalDetails>
+                        <div className="modal-close" onClick={() => setIsOpenModalRemove(false)}><i className="uil uil-times-circle" /></div>
+                        <div className="modal-content">
+                            <h3>Adicionar Saída</h3>
+                            <i>Saída são valores que saíram de sua conta</i>
+
+                            <label htmlFor="">Qual é o nome da despesa?</label>
+                            <input type="text" />
+
+                            <label htmlFor="">Informe a data</label>
+                            <input type="date" name="" id="" />
+
+                            <label htmlFor="">Informe o valor</label>
+                            <input type="text" name="" id="" />
+
+                            <label htmlFor="">Qual é a categoria?</label>
+                            <select name="" id="">
+                                <option value=""></option>
+                                <option value=""></option>
+                                <option value=""></option>
+                                <option value=""></option>
+                            </select>
+
+                            <div className="buttons">
+                                <button className="send">Enviar</button>
+                                <button className="clear">Limpar</button>
+                            </div>
+
+                        </div>
+                    </ModalDetails>
+                ) : null
+            }
         </SidebarMenuDetails>
     )
 }
@@ -152,4 +235,109 @@ const SidebarMenuDetails = styled.div`
         }
     }
 
+`
+
+const ModalDetails = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 9999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .modal-content {
+        background: var(--background);
+        border-radius: 8px;
+        padding: 20px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+        max-width: 80%; 
+        max-height: 80%;
+        overflow-y: auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        flex-direction: column;
+
+        h3 {
+            font-size: 1.8rem;
+        }
+
+        i {
+            font-size: 0.8rem;
+            color: #ccc;
+        }
+
+        label {
+            margin-top: 1rem;
+        }
+
+        input, select, input[type="date"] {
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+            font-size: 16px;
+            padding: 6px;
+            border: 1px solid var(--white);
+            border-radius: 5px;
+            outline: none;
+            transition: border-color 0.3s ease;
+            background: var(--background);
+            color: var(--white);
+        }
+
+        option {
+            text-align: center;
+        }
+
+        input:focus, select:focus, input[type="date"]:focus {
+            border-color: var(--white);
+        }
+
+        input.error, select.error, input[type="date"].error {
+            border-color: var(--red);
+        }
+
+        input[type="date"]::-webkit-calendar-picker-indicator {
+            filter: invert(50%);
+        }
+
+        button {
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+            font-size: 14px;
+            padding: 10px 20px;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            outline: none;
+            transition: background-color 0.3s ease;
+            margin-top: 1rem;
+            font-weight: bold;
+        }
+
+        .buttons {
+            display: flex;
+            flex-direction: row;
+        }
+        
+        .send {
+            background: var(--green);
+        }
+
+        .clear {
+            background: var(--orange);
+            margin-left: 0.5rem;
+        }
+    }
+
+    .modal-close {
+        position: absolute;
+        top: 30px;
+        right: 30px;
+        cursor: pointer;
+        font-size: 2rem;
+    }
 `
