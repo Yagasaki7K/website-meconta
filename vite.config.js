e59cb2f7340f3b7319f9c74980a7cddfe9c41073
Anderson Marlon <anderson18.marlon@gmail.com>
2024-05-01 20:06:04 -0300

    Added: Million - 01-05 20:06

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import million from 'million/compiler'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRefresh()],
+  plugins: [react(), reactRefresh(), million.vite({
+    auto: {
+      threshold: 0.05,
+      skip: ["useBadHook", /badVariable/g],
+    },
+  }),],
 })
